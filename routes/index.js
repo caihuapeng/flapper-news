45b9d3716c28bdb5c28dc01828ebb783132fb114
caihuapeng <caihuapeng@126.com>
2016-03-04 00:39:42 +0800

    Adding Authentication via Passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+var jwt = require('express-jwt');
+var auth = jwt({ secret: 'SECRET', userProperty: 'payload' });
 
 router.get('/posts', function(req, res, next) {
     Post.find(function(err, posts) {
@@ -14,8 +19,9 @@ router.get('/posts', function(req, res, next) {
         res.json(posts);
     });
 });
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function(err, post) {
         if (err) {
@@ -64,7 +70,7 @@ router.get('/posts/:post', function(req, res, next) {
         res.json(post);
     });
 });
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
     req.post.upvote(function(err, post) {
         if (err) {
             return next(err);
@@ -73,9 +79,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
         res.json(post);
     });
 });
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment) {
         if (err) {
@@ -92,7 +99,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
         });
     });
 });
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
     req.comment.upvote(function(err, comment) {
         if (err) {
             return next(err);
@@ -101,9 +108,49 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
         res.json(comment);
     });
 });
+
+router.post('/register', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password)
+
+    user.save(function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.json({ token: user.generateJWT() })
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+
+        if (user) {
+            return res.json({ token: user.generateJWT() });
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
